9a66c7f2b825ad59d2cbfd474b871f6096f6dae9
Viet Nguyen <vnguyen@redhat.com>
2017-03-14 15:05:44 +0000

    Use env for plugin dir

--- run.sh
+++ run.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
+
+source /etc/sysconfig/grafana-server
+
 if [ ! -z "${GF_INSTALL_PLUGINS}" ]; then
   OLDIFS=$IFS
   IFS=','
   for plugin in ${GF_INSTALL_PLUGINS}; do
-    grafana-cli --pluginsDir /usr/share/grafana/data/plugins plugins install ${plugin}
+    grafana-cli --pluginsDir ${PLUGINS_DIR} plugins install ${plugin}
   done
   IFS=$OLDIFS
 fi
-source /etc/sysconfig/grafana-server
+
 cd /usr/share/grafana
 /usr/sbin/grafana-server \
 --config=${CONF_FILE} \
